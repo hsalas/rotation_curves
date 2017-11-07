0e29a2b81d64b0ff41b8000502b972a457a4b07a
Hector Salas O <hector.salas.o@gmail.com>
2017-11-07 11:38:17 -0300

    added docstring to read_data.py

--- code/read_data.py
+++ code/read_data.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 
-# Read rotation curves data from file and passes to a QTable
+'''Script that reads data files with rotation curves of galaxies
+
+Author: Hector Salas <hector.salas.o@gmail.com>'
+'''
 
 from astropy.table import Table, QTable
 from astropy.io import ascii
@@ -19,21 +22,10 @@ def read_data_file(data_file, directory):
     '''
 
     colnames = ['radius','vgas', 'vdisk', 'vbulge', 'vobs', 'err-vobs', 'vu', 'vt', 'rxv', 'vxy']
-    # dtype = [u.quantity.Quantity, u.quantity.Quantity, u.quantity.Quantity, u.quantity.Quantity, u.quantity.Quantity, u.quantity.Quantity, u.quantity.Quantity, u.quantity.Quantity, u.quantity.Quantity, u.quantity.Quantity]
 
     table = ascii.read(directory + data_file, names=colnames)
     table = Table(table)
     qtable = QTable()
-    # table['radius'].units = u.kpc
-    # table['vgas'].units = u.km/u.s
-    # table['vdisk'].units = u.km/u.s
-    # table['vbulge'].units = u.km/u.s
-    # table['vobs'].units = u.km/u.s
-    # table['err-vobs'].units = u.km/u.s
-    # table['vu'].units = u.km/u.s
-    # table['vt'].units = u.km/u.s
-    # table['rxv'].units = u.kpc
-    # table['vxy'].units = u.km/u.s
     qtable['radius'] = table['radius']*u.kpc
     qtable['vgas'] = table['vgas']*u.km/u.s
     qtable['vdisk'] = table['vdisk']*u.km/u.s
@@ -44,5 +36,4 @@ def read_data_file(data_file, directory):
     qtable['vt'] = table['vt']*u.km/u.s
     qtable['rxv'] = table['rxv']*u.kpc
     qtable['vxy'] = table['vxy']*u.km/u.s
-    # import pdb;pdb.set_trace()
     return(qtable)
