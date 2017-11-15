solved error with units and fixed some misspelled code in iso.py

code/models/iso.py
@@ -20,9 +20,9 @@ def halo_iso(r, par_list):
             Radius in Kpc. Must be non negative.
 
         par_list:   list of length 3.
-                    Parameters of Einasto model.
+                    Parameters of ISO model.
                     par_list[0]:    float or int.
-                                    Central mass density Rho_0  in 10^-3 Msol/pc^3.
+                                    Central mass density in 10^-3 Msol/pc^3.
                     par_list[1]:    float or int.
                                     Core radius in Kpc.
     Outputs
@@ -35,9 +35,9 @@ def halo_iso(r, par_list):
         raise TypeError("type(r) must be array_like of int or float")
     if (r.all() < 0):
         raise ValueError("r values must be greater than 0")
-    elif type(par) != list:
+    elif type(par_list) != list:
         raise TypeError("type(par_list) must be list")
-    elif len(par) != 2:
+    elif len(par_list) != 2:
         raise IndexError("len(par_list) must be 2")
     elif (type(par_list[0]) != int and type(par_list[0]) != float):
         raise TypeError("type(par_list[0]) must be int or float")
@@ -45,14 +45,17 @@ def halo_iso(r, par_list):
         raise TypeError("type(par_list[1]) must be int or float")
     elif par_list[1] == 0:
         raise ZeroDivisionError("par_list[1] must be different from 0")
-    elif par_listp[0] < 0:
+    elif par_list[0] < 0:
         raise ValueError("par_list[0] cannot be negative")
-    elif par_listp[1] < 0:
+    elif par_list[1] < 0:
         raise ValueError("par_list[1] cannot be negative")
     else:
         pass
 
-    rho_0 = param[0]*10**(-3)*u.M_sun/(u.pc)**3
+    r = r*u.kpc
+    rho_0 = par_list[0]*10**(-3)*u.M_sun/(u.pc)**3
     rc = par_list[1]*u.kpc
-    v = np.sqrt(4*np.pi*G*rho_0*np.power(rc, 2)*(1.0-(rc/r)*np.arctan(r/rc)))
+    c = (rc/r).value
+    v = np.sqrt(4*np.pi*G*rho_0*np.power(rc, 2)*(1.0 - c*np.arctan(1/c)))
+    v = v.to(u.km/u.s)
     return(v)