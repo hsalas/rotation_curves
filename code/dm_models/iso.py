372f0f54cffcc69f879b7d4291cfeea1008cb2c0
Hector Salas O <hector.salas.o@gmail.com>
2017-12-03 19:14:13 -0300

    modified iso model to use astropy.fitting methods

--- code/dm_models/iso.py
+++ code/dm_models/iso.py
@@ -11,53 +11,42 @@ import numpy as np
 import astropy.units as u
 
 from astropy.constants import G, M_sun
+from astropy.modeling import Fittable1DModel, Parameter
 
 # ------- Functions -------
 
+class halo_iso(Fittable1DModel):
+    '''ISO dark mater halo model.
 
-def halo_iso(r, par_list):
-    '''ISO halo model
     Inputs:
-        r:  array_like
-            Radius in Kpc. Must be non negative.
+        r:  array_like.
+            Radius in Kpc.
 
-        par_list:   list of length 3.
-                    Parameters of ISO model.
-                    par_list[0]:    float or int.
-                                    Central mass density in 10^-3 Msol/pc^3.
-                    par_list[1]:    float or int.
-                                    Core radius in Kpc.
+        Parameters of ISO model.
+        rho_0:    float or int.
+            Central mass density in 10^-3 Msol/pc^3.
+        rc: float or int.
+            Core radius in Kpc.
     Outputs
-        v:
+        v:  array_like.
             velocity in km/s.
     '''
-    try:
-        np.array(r, float)
-    except ValueError:
-        raise TypeError("type(r) must be array_like of int or float")
-    if (r.all() < 0):
-        raise ValueError("r values must be greater than 0")
-    elif type(par_list) != list:
-        raise TypeError("type(par_list) must be list")
-    elif len(par_list) != 2:
-        raise IndexError("len(par_list) must be 2")
-    elif (type(par_list[0]) != int and type(par_list[0]) != float):
-        raise TypeError("type(par_list[0]) must be int or float")
-    elif (type(par_list[1]) != int and type(par_list[1]) != float):
-        raise TypeError("type(par_list[1]) must be int or float")
-    elif par_list[1] == 0:
-        raise ZeroDivisionError("par_list[1] must be different from 0")
-    elif par_list[0] < 0:
-        raise ValueError("par_list[0] cannot be negative")
-    elif par_list[1] < 0:
-        raise ValueError("par_list[1] cannot be negative")
-    else:
-        pass
+    inputs = ('r',)
+    outputs = ('v',)
 
-    r = r*u.kpc
-    rho_0 = par_list[0]*10**(-3)*u.M_sun/(u.pc)**3
-    rc = par_list[1]*u.kpc
-    c = (rc/r).value
-    v = np.sqrt(4*np.pi*G*rho_0*np.power(rc, 2)*(1.0 - c*np.arctan(1/c)))
-    v = v.to(u.km/u.s)
-    return(v)
+    rho_0 = Parameter(bounds=(1e-8, 1e3))
+    rc = Parameter(bounds=(1e-12, 300))
+    fit_deriv = None
+
+    @staticmethod
+    def evaluate(r, rho_0, rc):
+        '''ISO dark mater halo function.
+        '''
+        r = r*u.kpc
+        rho_0 = rho_0*10**(-3)*u.M_sun/(u.pc)**3
+        rc = rc*u.kpc
+        c = (rc/r).value
+        v = np.sqrt(4*np.pi*G*rho_0*np.power(rc, 2)*(1.0 - c*np.arctan(1/c)))
+        v = v.to(u.km/u.s)
+        v = v.value
+        return(v)
