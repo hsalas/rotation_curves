51d393033395be61e7c08cea74922740bd03b6e7
Hector Salas O <hector.salas.o@gmail.com>
2017-12-07 00:33:48 -0300

    added error handleling for user input in rotation curves.py

--- code/rotation_curves.py
+++ code/rotation_curves.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
 '''
-Script that adjust the rotation curve of a galaxy by with a dark matter halo model.
+Script that adjust the rotation curve of a galaxy by with a dark matter halo
+model.
 
 Author: Hector Salas O.
 '''
@@ -17,44 +18,76 @@ import argparse
 import plots
 import fits
 import logs
+import glob
 
 # ------- Args --------
 
-#get models in dm_models
-model_list = model_handler.get_model_list()
+# get models in dm_models
+model_list = model_handler.get_model_list(print_list='no')
+model_list = str(model_list)[1:-1]
 
 help_text = 'Fits rotation curves of a galaxy with a dark matter halo model.'
 sign_off = 'Author: Hector Salas O. <hector.salas.o@gmail.com>'
-
 parser = argparse.ArgumentParser(description=help_text, epilog=sign_off)
 
+# user input args.
 parser.add_argument('-f', '--File', type=str, default='ngc3198.dat', dest='file', metavar='FILE', help='(str) Name of file containing the rotation curve of the galaxy. Default value "ngc3198" ', action='store')
 parser.add_argument('-p', '--Path', type=str, default='../data/', dest='path', metavar='PATH', help='(str) Path of dorectory containing the data files. Default value "../data/".', action='store')
-parser.add_argument('-m', '--Model', type=str, default='nfw', dest='model', metavar='MODEL', help='''(str) Model to fit.
-Available models: '''+str(model_list)[1:-1]+'.', action='store')
-parser.add_argument('-o', '--Output', type=int, default=2, dest='output', metavar='OUTPUT', help='''Output of the results: 0 ->Save to file.
+parser.add_argument('-m', '--Model', type=str, default='nfw', dest='model', metavar='MODEL', help='(str) Model to fit. Available models: '+model_list+'.', action='store')
+parser.add_argument('-o', '--Output', type=int, default=1, dest='output', metavar='OUTPUT', help='''Output of the results: 0 ->Save to file.
                                                 1 ->Show on screen (default).''')
 
 arguments = parser.parse_args()
-data_file = arguments.file
-directory = arguments.path
-model_name = arguments.model
-out = arguments.output
 
 # ------- Functions -------
 
 
+def check_user_args(arguments, model_list):
+    '''Function to check for errors user args. If no error returns the user args
+    variables, otherwise it raises the corresponding errors.
+
+    Inputs:
+        arguments:  <class 'argparse.Namespace'>.
+                    User args.
+        model_list: str.
+                    String with the list of available models.
+    Outputs:
+        user_args:  tuple.
+                    Tuple with the user args.
+                    (out, model_name, data_file, directory)
+    '''
+    out = arguments.output
+    model_name = arguments.model
+    data_file = arguments.file
+    directory = arguments.path
+
+    model_name.lower()
+    if (out != 1) and (out != 0):
+        raise ValueError('OUTPUT must be either 1 or 0')
+    if model_name not in model_list:
+        raise ValueError('''Invalid model name. Chek list of available
+            models:'''+model_list)
+    if not glob.os.path.isdir(directory):
+        raise ValueError(f'Directory {directory} does not exists.')
+    if not glob.os.path.isfile(directory+data_file):
+        raise ValueError(f'File {directory}{data_file} does not exists.')
+    user_args = (out, model_name, data_file, directory)
+    return(user_args)
+
+
 def main():
-    #read data.
-    data = read_data.read_data(data_file, directory)
-    #call to model.
-    model = model_handler.model_caller(model_name)
-    #fit model to data.
+    # check if user args are velid.
+    user_args = check_user_args(arguments, model_list)
+    # read data.
+    data = read_data.read_data(user_args[2], user_args[3])
+    # call to model.
+    model = model_handler.model_caller(user_args[1])
+    # fit model to data.
     result = fits.fit(data, model[0])
-    #plot of fitted model.
-    plots.plot(data, result[0], model[1], data_file, show=out)
-    #report of fit results.
-    logs.log(data_file, model[1], result, show=out)
+    # plot of fitted model.
+    plots.plot(data, result[0], model[1], user_args[2], show=user_args[0])
+    # report of fit results.
+    logs.log(user_args[2], model[1], result, show=user_args[0])
 
 # ------------MAIN------------
 
